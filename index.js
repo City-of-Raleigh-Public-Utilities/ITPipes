fa3f8fda52ea59ea5eaf2f8cf3c9dc575ae626a6
cwhite911 <smortopahri@gmail.com>
2015-07-09 18:08:24 -0400

    Added basic quering ability

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 var sql = require('mssql'),
     local = require('./modules/local');
+
+
+
+var connection = new sql.Connection(local.SQLSERVER);
+
+
+connection.connect(function(err) {
+    console.log(err);
+    var request = new sql.Request(connection);
+    request.query(local.QUERIES.findAllTables, function(err, recordset) {
+       console.dir(recordset);
+       console.log(recordset.length); // count of recordsets returned by the procedure
+
+   });
+});
