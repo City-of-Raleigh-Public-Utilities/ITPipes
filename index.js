8bf99001aa465cda351c37a6f463aec9a5d49bbc
cwhite911 <smortopahri@gmail.com>
2015-07-10 15:41:50 -0400

    Added search by view

--- index.js
+++ index.js
@@ -9,8 +9,10 @@ var connection = new sql.Connection(local.SQLSERVER);
 connection.connect(function(err) {
     console.log(err);
     var request = new sql.Request(connection);
-    request.query(local.QUERIES.findAllTables, function(err, recordset) {
+    request.query(local.QUERIES.view, function(err, recordset) {
+
        console.dir(recordset);
+       console.log(typeof recordset);
        console.log(recordset.length); // count of recordsets returned by the procedure
 
    });
